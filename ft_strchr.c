e9d2a455c91ef5f51b859e7815483d3a8dd13152
Kaietan <34032203+kaietanpaul@users.noreply.github.com>
2024-03-08 15:40:44 +0000

    included libft.h for size_t

--- ft_strchr.c
+++ ft_strchr.c
@@ -10,7 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include "libft.h"
 
 char	*ft_strchr(const char *s, int c)
--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -10,6 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 size_t	ft_strlcpy(char *dest, const char *src, size_t dest_size)
 {
 	size_t	i;
