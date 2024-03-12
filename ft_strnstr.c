7bffabc8905e8cf7fefc80a8f769fd4de37d3517
Kajetan Paul <kpaul@c1r3s3.42warsaw.pl>
2024-03-12 12:13:24 +0100

    strnstr - norminette update

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -2,26 +2,27 @@
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-    unsigned int index_hay;
-    unsigned int index_need;
+	unsigned int	index_hay;
+	unsigned int	index_need;
 
-    index_hay = 0;
-    index_need = 0;
+	index_hay = 0;
+	index_need = 0;
 	if (needle[0] == '\0')
 		return ((char *)(haystack));
 	while (index_hay < len)
-        {
-            if (haystack[index_hay] == needle[index_need])
-            {
-                if ((ft_strlen(needle) - 1) == (int)index_need)
-                    return ((char *)haystack + index_hay - index_need);
-                index_hay ++;
-                index_need ++;
-            } else
-            {
-                index_hay ++;
-                index_need = 0;
-            }
-        }
-        return (NULL);
+	{
+		if (haystack[index_hay] == needle[index_need])
+		{
+			if ((ft_strlen(needle) - 1) == (int)index_need)
+				return ((char *)haystack + index_hay - index_need);
+			index_hay ++;
+			index_need ++;
+		}
+		else
+		{
+			index_hay ++;
+			index_need = 0;
+		}
+	}
+	return (NULL);
 }
