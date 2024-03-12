c18c57b92289fba2ad9f788ea0ba0a2854811d68
Kajetan Paul <kpaul@c1r3s3.42warsaw.pl>
2024-03-12 17:05:04 +0100

    norminette updates

--- ft_atoi.c
+++ ft_atoi.c
@@ -2,15 +2,15 @@
 
 int	ft_atoi(const char *str)
 {
-	int	new_num;
-	char	neg_pos;
-	const char *p;
+	int			new_num;
+	char		neg_pos;
+	const char	*p;
 
 	new_num = 0;
 	neg_pos = '+';
 	p = str;
-	while (*p == ' ' || *p == '\n' || *p == '\t' ||
-		*p == '\v' || *p == '\f' || *p == '\r')
+	while (*p == ' ' || *p == '\n' || *p == '\t'
+		|| *p == '\v' || *p == '\f' || *p == '\r')
 		p++;
 	if (*p == '-' || *p == '+')
 	{
@@ -18,11 +18,11 @@ int	ft_atoi(const char *str)
 		p++;
 	}
 	while (ft_isdigit(*p))
-		{
-			new_num *= 10;
-			new_num += (*p - '0');
-			p++;
-		}
+	{
+		new_num *= 10;
+		new_num += (*p - '0');
+		p++;
+	}
 	if (neg_pos == '-')
 		new_num *= -1;
 	return (new_num);
