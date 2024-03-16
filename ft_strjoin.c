edb9155df5002ad8c6fc5f7593eed0ef5323d5b1
Kaietan <34032203+kaietanpaul@users.noreply.github.com>
2024-03-16 10:43:20 +0000

    strjoin - norminette update

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -4,17 +4,13 @@
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*ptr;
-	size_t	new_size;
 	int	i;
-	int	j;
 
-	new_size = ft_strlen(s1) + ft_strlen(s2);
 	i = 0;
-	j = ft_strlen(s1);
-	ptr = malloc(new_size);
+	ptr = malloc(ft_strlen(s1) + ft_strlen(s2));
 	if (ptr)
 	{
-		while (i < j)
+		while (i < ft_strlen(s1))
 		{
 			ptr[i] = s1[i];
 			i++;
@@ -22,8 +18,8 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		i = 0;
 		while (i < ft_strlen(s2))
 		{
-			ptr[j] = s2[i];
-			j++;
+			ptr[ft_strlen(s2) + i] = s2[i];
+			ft_strlen(s2++);
 			i++;
 		}
 	}
