d103d806d0b20e4b0af2a073abbeb29d0508845d
Kaietan <34032203+kaietanpaul@users.noreply.github.com>
2024-03-19 18:33:47 +0000

    putnbr - update

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -14,17 +14,21 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	*string;
-	int		i;
+	char	number;
 
-	i = 0;
-	string = ft_itoa(n);
-	if (string != NULL)
+	number = 0;
+	if (n == -2147483648)
+		write(fd, "-2147483648", 11);
+	else
 	{
-		while (string[i])
+		if (n < 0)
 		{
-			write(fd, &string[i], 1);
-			i++;
+			write(fd, "-", 1);
+			n = -n;
 		}
+		if (n > 9)
+			ft_putnbr_fd(n / 10, fd);
+		number = n % 10 + '0';
+		write(fd, &number, 1);
 	}
 }
