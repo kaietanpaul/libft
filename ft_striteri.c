e83e9ee049f9babf56b651e3c366e5dbdda597b3
Kaietan <34032203+kaietanpaul@users.noreply.github.com>
2024-03-19 14:44:27 +0000

    striteri - apply xFunction to all char in string (in-place, void)

---
+++ ft_striteri.c
@@ -0,0 +1,35 @@
+#include "libft.h"
+#include <stdlib.h>
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+	int	i;
+
+	i = 0;
+	if (s != NULL)
+	{
+		while (s[i])
+		{
+			f(i, &s[i]);
+			i++;
+		}
+	}
+}
+/*#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+static void uppercase_char(unsigned int index __attribute__((unused)), char *ch) {
+	if (ch != NULL) {
+		*ch = toupper(*ch);
+	}
+}
+int main(void) {
+	char *str = malloc(sizeof(char) * (ft_strlen("Hello, World!") + 1));
+
+	strcpy(str, "Hello, World!");
+	ft_striteri(str, uppercase_char);
+	printf("Modified string: %s\n", str);
+	free(str);
+
+	return 0;
+}*/
